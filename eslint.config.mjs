f08f27daaf1af427bccc7cf2a87f3af5a2bf3c82
Céline Souchet <souchet.celine.5@gmail.com>
2026-04-01 19:15:05 +0200

    Add new ESLint plugins: - eslint-import-resolver-typescript - eslint-plugin-import - eslint-plugin-unused-imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,33 +2,91 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
 import nextTs from 'eslint-config-next/typescript';
 import prettierConfig from 'eslint-config-prettier/flat';
+import importPlugin from 'eslint-plugin-import';
 import prettierPlugin from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
 
-  // Prettier run via ESLint
   {
     plugins: {
       prettier: prettierPlugin,
+      import: importPlugin,
+      'unused-imports': unusedImports,
     },
+
     rules: {
       'prettier/prettier': 'error',
+
+      'import/newline-after-import': 'warn',
+
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'type',
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+          ],
+
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: 'next/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@mantine/**',
+              group: 'external',
+              position: 'after',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+
+          pathGroupsExcludedImportTypes: ['builtin'],
+
+          'newlines-between': 'always',
+
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      'unused-imports/no-unused-imports': 'error',
+
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: true,
+      },
     },
   },
 
-  // Disable conflicts with ESLint
+  // Disable ESLint rules that conflict with Prettier
   prettierConfig,
 
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
