3ef5883a7da07f39c7e9e88064531451471aa186
Céline Souchet <souchet.celine.5@gmail.com>
2026-07-10 14:15:34 +0200

    Enable malt profile

--- src/data/freelance.ts
+++ src/data/freelance.ts
@@ -1,14 +1,14 @@
 // import { IconBrandFiverr, IconBrandUpwork } from '@tabler/icons-react';
-// import { SiMalt } from 'react-icons/si';
+import { SiMalt } from 'react-icons/si';
 
 import { Link } from '@/types/link';
 
 export const freelanceLinks: Link[] = [
-  /*  {
+  {
     name: 'Malt',
-    url: 'https://www.malt.fr/profile/your-profile',
+    url: 'https://www.malt.fr/profile/celinesouchet',
     icon: SiMalt,
-  },*/
+  },
   /*  {
     name: 'Upwork',
     url: 'https://www.upwork.com/freelancers/your-profile',
